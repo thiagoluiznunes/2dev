21faf528dd19f0a478bc6d4b51a855ef33424614
ThiagoLuizNunes <thiagoluiz.dev@gmail.com>
2018-11-19 15:46:06 -0300

    create user service and arrange environment variables

--- ci-app/src/environments/environment.prod.ts
+++ ci-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  appName: 'CI Achados e Perdidos',
+  version: '1.0',
+  owner: 'Thiago Luiz Nunes',
+  year: '2018',
+  site: 'https://github.com/ThiagoLuizNunes',
+  apiUrl: '',
+  oapiUrl: '',
+  userKey: '_ci-achados-perdidos_'
 };
