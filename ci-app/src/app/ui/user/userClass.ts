ff7f622c3addfdd8d451468d4b8b74aaa5130326
ThiagoLuizNunes <thiagoluiz.dev@gmail.com>
2018-11-21 00:47:55 -0300

    fix style guide

--- ci-app/src/app/ui/user/userClass.ts
+++ ci-app/src/app/ui/user/userClass.ts
@@ -3,7 +3,7 @@ class UserClass {
   userEmail: string;
   userToken: string;
 
-  constructor(name: string, email: string, token:string) {
+  constructor(name: string, email: string, token: string) {
     this.userName = name;
     this.userEmail = email;
     this.userToken = token;
