67fae04bb392594905ae6798100ed4925138cd00
ThiagoLuizNunes <thiagoluiz.dev@gmail.com>
2018-11-22 12:07:29 -0300

    add httpclientmodule dependency to project

--- ci-app/src/app/ui/user/user.component.ts
+++ ci-app/src/app/ui/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'ci-user',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
 
+  test(): void {
+    this.userService.test();
+  }
 }
