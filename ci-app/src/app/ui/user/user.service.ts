428f9dd17b7a7b89db832a256e084250ec6e0ba5
ThiagoLuizNunes <thiagoluiz.dev@gmail.com>
2018-11-22 00:29:03 -0300

    create submit method

--- ci-app/src/app/ui/user/user.service.ts
+++ ci-app/src/app/ui/user/user.service.ts
@@ -14,10 +14,35 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUser() {
+  getUser(): void {
     if (!this.user) {
       this.user = new UserClass('asd', 'asd', 'asd');
       // user = new User(JSON.parse(localStorage.getItem(environment.ci_userKey));
     }
+  }
+
+  test(): void {
+    this.http.get('https://jsonplaceholder.typicode.com/posts/1')
+      .subscribe(
+        response => {
+          console.log('Response: ', response);
+        },
+        error => {
+          console.log('ERROR ', error);
+        }
+      );
+  }
+
+  submit(url: string, user: UserClass, callback: () => void): void {
+
+    this.http.post<UserClass>(url, JSON.stringify(user))
+      .subscribe(
+        response => {
+          console.log('POST REQUEST is successful', response);
+        },
+        error => {
+          console.log('ERROR', error);
+        }
+      );
   }
 }
