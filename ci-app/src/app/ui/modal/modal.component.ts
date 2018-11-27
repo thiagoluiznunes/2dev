c8bb21946cadde5f6a15404e6d9a6ce218571691
ThiagoLuizNunes <thiagoluiz.dev@gmail.com>
2018-11-26 23:57:55 -0300

    create modal component

---
+++ ci-app/src/app/ui/modal/modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ci-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
