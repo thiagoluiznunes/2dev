1147683eee20480d53d156d55039621da3d91a33
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-05-23 22:20:48 -0300

    update home routing and module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,9 +5,15 @@ import { HomeComponent } from './home.component';
 import { UiModule} from '../ui/ui.module';
 import { HomeRoutingModule } from './home.routing.module';
 import { UserDetailResolver } from 'src/app/core/guards/user.detail.guard';
+import { MainComponent } from './main/main.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    MainComponent,
+    ContactComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule,
--- src/app/modules/home/home.routing.module.ts
+++ src/app/modules/home/home.routing.module.ts
@@ -1,13 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { HomeComponent } from './home.component';
-import { UserDetailResolver } from 'src/app/core/guards/user.detail.guard';
+import { MainComponent } from './main/main.component';
+import { ContactComponent } from './contact/contact.component';
 
 const homeRoutes: Routes = [
-  { path: '', component: HomeComponent, resolve: { user: UserDetailResolver }, },
-  { path: '**', pathMatch: 'full', redirectTo: '' },
+  // {
+  //   path: 'main',
+  //   loadChildren: './main/main.module#MainModule',
+  // },
+  // {
+  //   path: 'contact',
+  //   loadChildren: './contact/contact.module#ContactModule',
+  // },
+  // {
+  //   path: '',
+  //   resolve: { user: UserDetailResolver },
+  //   loadChildren: './main/main.module#MainModule'
+  // },
+  // {
+  //   path: '**',
+  //   pathMatch: 'full',
+  //   redirectTo: ''
+  // },
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'main' },
+      { path: 'main', component: MainComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: '**', pathMatch: 'full', redirectTo: 'main' },
+    ]
+  }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(homeRoutes)],
