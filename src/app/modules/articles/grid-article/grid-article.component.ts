87dc1cf70078dfec0ed8c488ee1f0b65382f5040
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-05-24 21:31:01 -0300

    create grid-article component

---
+++ src/app/modules/articles/grid-article/grid-article.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-article',
+  templateUrl: './grid-article.component.html',
+  styleUrls: ['./grid-article.component.css']
+})
+export class GridArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
