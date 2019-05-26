7576d31a73dd520bbd839a8600d0b826b3771602
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-05-25 21:32:16 -0300

    create row-article component

---
+++ src/app/modules/articles/row-article/row-article.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-row-article',
+  templateUrl: './row-article.component.html',
+  styleUrls: ['./row-article.component.css']
+})
+export class RowArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
