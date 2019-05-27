654ad5d717b71c769ae2aa51d651ed133ba341c0
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-05-27 17:04:38 -0300

    fix mostRecentArticles layout

--- src/app/modules/home/main/main.component.ts
+++ src/app/modules/home/main/main.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  mostRatedArticles: Object = [
+    '',
+    '',
+    ''
+  ];
   constructor() { }
 
   ngOnInit() {
