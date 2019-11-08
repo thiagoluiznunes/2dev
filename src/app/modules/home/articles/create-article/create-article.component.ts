236a4fc8ae16ce9ddfed4ceba92aa9e0fe257ca9
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-11-08 00:00:38 -0300

    create observable to titleTextArea

--- src/app/modules/home/articles/create-article/create-article.component.ts
+++ src/app/modules/home/articles/create-article/create-article.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-create-article',
   templateUrl: './create-article.component.html',
   styleUrls: ['./create-article.component.css']
 })
-export class CreateArticleComponent implements OnInit {
+export class CreateArticleComponent implements OnInit, AfterViewInit {
 
   @ViewChild('titleTextArea', { static: false }) titleTextArea: ElementRef;
   title: String = '';
@@ -16,9 +19,19 @@ export class CreateArticleComponent implements OnInit {
   }
 
   onKeyTitle(event: any) { // without type info
-    if (event.key !== 'ENTER' || event.keyCode === 13 ) {
-      event.preventDefault();
+    if (event.key !== 'ENTER' || event.keyCode === 13) {
     }
+  }
+
+  ngAfterViewInit() {
+    const obs = fromEvent(this.titleTextArea.nativeElement, 'keyup')
+      .pipe(
+        map((e: any) => e.target.value), // extract the value of the input
+        debounceTime(5000),
+        distinctUntilChanged()
+      ).subscribe(data => {
+        console.log(data);
+      });
   }
 
 }
