3ae34c5027a86426ff64e60278f48ef2dd39f152
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-11-09 00:19:38 -0300

    update title css

--- src/app/modules/home/articles/create-article/create-article.component.ts
+++ src/app/modules/home/articles/create-article/create-article.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, Renderer2, OnChanges, SimpleChanges, HostListener } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
-
 
 @Component({
   selector: 'app-create-article',
@@ -13,33 +12,44 @@ export class CreateArticleComponent implements OnInit, AfterViewInit {
   @ViewChild('titleSpan', { static: false }) titleSpan: ElementRef;
   @ViewChild('titleTextArea', { static: false }) titleTextArea: ElementRef;
   title: String = '';
+  placeHolder: String = 'Título';
 
   constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
   }
-
-  onKeyTitle(event: any) { // without type info
-    if (event.key !== 'ENTER' || event.keyCode === 13) {
-    }
-  }
-
+  // if (event.key === 'Enter') {
+  //   this.titleTextArea.nativeElement.rows += 1;
+  // }
   ngAfterViewInit() {
-    const obs = fromEvent(this.titleTextArea.nativeElement, 'keyup')
+    fromEvent(this.titleTextArea.nativeElement, 'keyup')
       .pipe(
         map((e: any) => e.target.value), // extract the value of the input
         debounceTime(5000),
         distinctUntilChanged()
       ).subscribe(data => {
-        console.log(data);
+        console.log('Observable fromEvent: ', data);
       });
   }
 
   focusFunction(): void {
     this.renderer.setStyle(this.titleSpan.nativeElement, 'opacity', '1');
+    this.placeHolder = '';
   }
 
   focusOutFunction(): void {
     this.renderer.setStyle(this.titleSpan.nativeElement, 'opacity', '0');
+    this.placeHolder = 'Título';
   }
+
+  // @HostListener('document:click', ['$event'])
+  // onClickOutside($event) {
+  //   const titleClicked = this.titleH1.nativeElement.contains($event.target);
+  //   if (!titleClicked) {
+  //     this.renderer.setStyle(this.titleSpan.nativeElement, 'opacity', '0');
+  //   }
+  // }
+  // titleClick(): void {
+  //   this.renderer.setStyle(this.titleSpan.nativeElement, 'opacity', '1');
+  // }
 }
