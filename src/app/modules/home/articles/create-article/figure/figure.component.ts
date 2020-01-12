c91e99bd2d74ecb347a01772035d839577ff5b66
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2020-01-11 21:25:18 -0300

    update figureComponent css

--- src/app/modules/home/articles/create-article/figure/figure.component.ts
+++ src/app/modules/home/articles/create-article/figure/figure.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, ChangeDetectorRef, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, ChangeDetectorRef, Input, Renderer2, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-figure',
@@ -10,7 +10,10 @@ export class FigureComponent implements OnInit, AfterViewInit {
   @ViewChild('divFigureRef', { static: false }) divFigureRef: ElementRef;
   @ViewChild('imgFigureRef', { static: false }) imgFigureRef: ElementRef;
 
-  constructor() {
+  borderStatus: boolean;
+
+  constructor(private renderer: Renderer2) {
+    this.borderStatus = false;
   }
 
   ngOnInit() {
@@ -18,5 +21,16 @@ export class FigureComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.imgFigureRef.nativeElement.src = '/assets/imgs/profile.jpeg';
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside($event) {
+    const clickedInside = this.imgFigureRef.nativeElement.contains($event.target);
+    if (clickedInside) {
+      this.renderer.setStyle(this.imgFigureRef.nativeElement, 'border-color', '#1976d2');
+      this.borderStatus = true;
+    } else if (!clickedInside && this.borderStatus) {
+      this.renderer.setStyle(this.imgFigureRef.nativeElement, 'border-color', 'transparent');
+    }
   }
 }
